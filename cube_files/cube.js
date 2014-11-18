608fa7a410cfd73a1bbaccd26ec79f40a4de9e95
Anthony-Handwerker <antrog.werker@yahoo.com>
2014-11-17 19:19:05 -0500

    File importing works!

--- cube_files/cube.js
+++ cube_files/cube.js
@@ -2,7 +2,7 @@
 var canvas;
 var gl;
 
-var NumVertices  = 128;
+var NumVertices  = 0;
 var program;
 
 var points = [];
@@ -25,24 +25,31 @@ var lastMouseX = null;
 var lastMouseY = null;
 
 var vertices = [
-        vec4( -0.5, -0.5,  0.5, 0.3 ),
-        vec4( -0.5,  0.5,  0.5, 0.3 ),
-        vec4(  0.5,  0.5,  0.5, 0.3 ),
-        vec4(  0.5, -0.5,  0.5, 0.3 ),
-        vec4( -0.5, -0.5, -0.5, 0.3 ),
-        vec4( -0.5,  0.5, -0.5, 0.3 ),
-        vec4(  0.5,  0.5, -0.5, 0.3 ),
-        vec4(  0.5, -0.5, -0.5, 0.3 ),
-        vec4( -0.5, -0.5,  0.5, -0.3 ),
-        vec4( -0.5,  0.5,  0.5, -0.3 ),
-        vec4(  0.5,  0.5,  0.5, -0.3 ),
-        vec4(  0.5, -0.5,  0.5, -0.3 ),
-        vec4( -0.5, -0.5, -0.5, -0.3 ),
-        vec4( -0.5,  0.5, -0.5, -0.3 ),
-        vec4(  0.5,  0.5, -0.5, -0.3 ),
-        vec4(  0.5, -0.5, -0.5, -0.3 )
+        vec4( -0.5, -0.5,  0.5, 0.5 ),
+        vec4( -0.5,  0.5,  0.5, 0.5 ),
+        vec4(  0.5,  0.5,  0.5, 0.5 ),
+        vec4(  0.5, -0.5,  0.5, 0.5 ),
+        vec4( -0.5, -0.5, -0.5, 0.5 ),
+        vec4( -0.5,  0.5, -0.5, 0.5 ),
+        vec4(  0.5,  0.5, -0.5, 0.5 ),
+        vec4(  0.5, -0.5, -0.5, 0.5 ),
+        vec4( -0.5, -0.5,  0.5, -0.5 ),
+        vec4( -0.5,  0.5,  0.5, -0.5 ),
+        vec4(  0.5,  0.5,  0.5, -0.5 ),
+        vec4(  0.5, -0.5,  0.5, -0.5 ),
+        vec4( -0.5, -0.5, -0.5, -0.5 ),
+        vec4( -0.5,  0.5, -0.5, -0.5 ),
+        vec4(  0.5,  0.5, -0.5, -0.5 ),
+        vec4(  0.5, -0.5, -0.5, -0.5 )
+        /*vec4(-0.2, -0.2, 0.34641016151, -0.2),
+        vec4(-0.2, -0.2, -0.34641016151, -0.2),
+        vec4(-0.2, 0.4, 0.0, -0.2),
+        vec4(0.34641016151, 0.0, 0.0, -0.2),
+        vec4(0.0,0.0,0.0, 0.34641016151)*/
 
     ];
+
+var edges = [];
 
 var vertices2 = [];
 
@@ -50,6 +57,7 @@ var vertices2 = [];
 
 window.onload = function init()
 {
+
     canvas = document.getElementById( "gl-canvas" );
 
     canvas.onmousedown = handleMouseDown;
@@ -95,6 +103,54 @@ window.onload = function init()
     document.getElementById( "Toggle_Rotate" ).onclick = function () {
         is_rotating = !is_rotating;
     };
+
+    var fileInput = document.getElementById("model");
+    fileInput.addEventListener('change', function(e)
+    {
+        var file = fileInput.files[0];
+
+        var reader = new FileReader();
+
+        reader.onload = function(e) 
+        {
+            var input = reader.result;
+            var vals = input.split("\n");
+            vertices = [];
+            edges = [];
+            NumVertices = 0;
+            var flag = false;
+            for(var i = 0; i < vals.length; i++)
+            {
+                if(vals[i].trim() == "VERTICES")
+                {
+                    flag = false;
+                }
+                else if(vals[i].trim() == "EDGES")
+                {
+                    flag = true;
+                }
+                else if(flag)
+                {
+                    var line = vals[i].split(" ");
+                    var newEdge = [parseInt(line[0]), parseInt(line[1])];
+                    edges.push(newEdge);
+                    console.log(newEdge);
+                    NumVertices += 2;
+                }
+                else
+                {
+                    var line = vals[i].split(" ");
+                    var newVertex = [parseFloat(line[0]), parseFloat(line[1]), parseFloat(line[2]), parseFloat(line[3])];
+                    vertices.push(newVertex);
+                }
+            }
+            theta = [0,0,0];
+            theta2 = 0.0;
+            is_rotating = false;
+        }
+
+        reader.readAsText(file);
+    });
     
     render();
 }
@@ -143,7 +199,7 @@ function colorCube()
         
         temp.push(1.0);
 
-        var temp2 = rotate_point4d([[0, 0, 0, 0, 1], [1, 0, 1, 0], [0, 1, 0, 0]], theta2, temp);
+        var temp2 = rotate_point4d([[0, 0, 0, 0, 1], [1, 0, 0, 0], [0, 1, 0, 0]], theta2, temp);
         var temp3 = temp2.pop();
         temp2[0] /= temp3;
         temp2[1] /= temp3;
@@ -151,22 +207,30 @@ function colorCube()
         temp2[3] /= temp3;
         vertices2.push(temp2);
     }
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
-    quad( 9, 8, 11, 10 );
-    quad( 10, 11, 15, 14 );
-    quad( 11, 8, 12, 15 );
-    quad( 14, 13, 9, 10 );
-    quad( 12, 13, 14, 15 );
-    quad( 13, 12, 8, 9 );
-    quad( 13, 12, 4, 5);
-    quad( 8, 9, 1, 0);
-    quad( 11, 10, 2, 3);
-    quad( 15, 14, 6, 7);
+    for(var i = 0; i < edges.length; i++)
+    {
+        line(edges[i][0], edges[i][1]);
+    }
+}
+
+function line(a, b)
+{
+    var indices = [a, b];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        var temp = vertices2[indices[i]];
+        //var mult = (temp[3] + 1.0);
+
+        var mult = Math.pow(3.0,temp[3]);
+
+        var temp2 = vec3(mult * temp[0] * 0.6, mult * temp[1] * 0.6, mult * temp[2] * 0.6);
+        points.push( temp2 );
+        colors.push( vec4(mult*0.5, (1 - mult*0.5), 0.0, 1.0) );
+    
+        // for solid colored faces use 
+        //colors.push(vertexColors[a]);
+        
+    }
 }
 
 function quad(a, b, c, d) 
@@ -182,7 +246,7 @@ function quad(a, b, c, d)
 
         var mult = Math.pow(3.0,temp[3]);
 
-        var temp2 = vec3(mult * temp[0] * 0.7, mult * temp[1] * 0.7, mult * temp[2] * 0.7);
+        var temp2 = vec3(mult * temp[0] * 0.6, mult * temp[1] * 0.6, mult * temp[2] * 0.6);
         points.push( temp2 );
         colors.push( vec4(mult*0.5, (1 - mult*0.5), 0.0, 1.0) );
     
